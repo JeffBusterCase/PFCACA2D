083607c4eeaa587acf12a7ac934926933afac650
Jefferson David Silva Bomfim <jefferson_bomfim@outlook.com>
2018-07-12 14:49:27 +0000

    TS last update.

--- pfcaca2d_v0_6.js
+++ pfcaca2d_v0_6.js
@@ -1,5 +1,45 @@
 // PFCACA2D version 0.6
-var Point = (function () {
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+// Util
+function sleep(ms) {
+    return new Promise(function (resolve) { return setTimeout(resolve, ms); });
+}
+// PFCACA2D
+var Point = /** @class */ (function () {
     function Point(x, y, color, size) {
         if (size === void 0) { size = 1; }
         this.x = x;
@@ -9,7 +49,7 @@ var Point = (function () {
     }
     return Point;
 }());
-var Frame = (function () {
+var Frame = /** @class */ (function () {
     function Frame(frame) {
         this.frame = frame;
     }
@@ -41,7 +81,7 @@ var Frame = (function () {
     ;
     return Frame;
 }());
-var CanvasSprite = (function () {
+var CanvasSprite = /** @class */ (function () {
     // Start Position must be x=0, y=0
     function CanvasSprite(sprite) {
         this.sprite = sprite;
@@ -50,7 +90,7 @@ var CanvasSprite = (function () {
     return CanvasSprite;
 }());
 ;
-var Canvas = (function () {
+var Canvas = /** @class */ (function () {
     function Canvas(id, w, h) {
         if (w === void 0) { w = 0; }
         if (h === void 0) { h = 0; }
@@ -113,7 +153,7 @@ var Canvas = (function () {
     };
     return Canvas;
 }());
-var Animation = (function () {
+var Animation = /** @class */ (function () {
     function Animation(animationTime, _function) {
         this.animationTime = animationTime;
         this._function = _function;
@@ -139,36 +179,63 @@ var Animation = (function () {
     return Animation;
 }());
 ;
-var CanvasAnimation2D = (function () {
+var CanvasAnimation2D = /** @class */ (function () {
     function CanvasAnimation2D(canvasId, frames) {
         this.canvasId = canvasId;
         this.frames = frames;
     }
     ;
-    CanvasAnimation2D.prototype.play = function (time) {
+    CanvasAnimation2D.prototype.play = function (time, fps) {
         if (time === void 0) { time = this.frames.length; }
         this._canvas = new Canvas(this.canvasId);
         this._frameNumber = 0;
-        this._play(time);
+        this._play(time, fps);
     };
     ;
-    CanvasAnimation2D.prototype._play = function (time) {
+    CanvasAnimation2D.prototype._play = function (time, fps) {
         var self = this;
+        if (self.is_playing) {
+            window.cancelAnimationFrame(self._inter);
+        }
+        self.is_playing = true;
         var _last = self.frames[0];
         self._animationTime = self.frames.length;
-        self._inter = setInterval(reDraw, time);
+        var must_pass = 1.0 / fps;
+        var delta;
+        var last_time = Date.now() / 1000;
+        self._inter = window.requestAnimationFrame(reDraw);
         function reDraw() {
-            if (self._animationTime == 0) {
-                clearInterval(self._inter);
-            }
-            else {
-                // Draw frames
-                self._canvas.clearAll();
-                _last = self._canvas.draw(self.frames[self._frameNumber]);
-                self._frameNumber += 1;
-                self._animationTime -= 1;
-            }
-            ;
+            return __awaiter(this, void 0, void 0, function () {
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            delta = (Date.now() / 1000) - last_time;
+                            delta = delta >= 0 ? delta : 0;
+                            if (!(time <= 0)) return [3 /*break*/, 1];
+                            window.cancelAnimationFrame(self._inter);
+                            self.is_playing = false;
+                            return [3 /*break*/, 4];
+                        case 1:
+                            // Draw frames
+                            self._canvas.clearAll();
+                            _last = self._canvas.draw(self.frames[self._frameNumber]);
+                            self._frameNumber += 1;
+                            if (!(delta <= must_pass)) return [3 /*break*/, 3];
+                            return [4 /*yield*/, sleep((must_pass - delta) * 1000)];
+                        case 2:
+                            _a.sent();
+                            _a.label = 3;
+                        case 3:
+                            time -= must_pass;
+                            last_time = Date.now() / 1000;
+                            self._inter = window.requestAnimationFrame(reDraw);
+                            _a.label = 4;
+                        case 4:
+                            ;
+                            return [2 /*return*/];
+                    }
+                });
+            });
         }
         ;
     };
