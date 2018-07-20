2cd6852f2bf0fd01eb2c45df990c140adc367230
Jefferson David Silva Bomfim <jefferson_bomfim@outlook.com>
2018-07-20 21:26:54 +0000

    last TS update

--- pfcaca2d_v0_6.js
+++ pfcaca2d_v0_6.js
@@ -14,8 +14,8 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
         while (_) try {
-            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [0, t.value];
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
             switch (op[0]) {
                 case 0: case 1: t = op; break;
                 case 4: _.label++; return { value: op[1], done: false };
@@ -92,21 +92,118 @@ var CanvasSprite = /** @class */ (function () {
 ;
 var Vertex = /** @class */ (function () {
     function Vertex(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
         this.x = parseFloat(x);
         this.y = parseFloat(y);
         this.z = parseFloat(z);
     }
     return Vertex;
 }());
+function getFaceMedianPosition(face) {
+    var x = 0, y = 0, z = 0, i = 0;
+    for (var _a = 0, face_1 = face; _a < face_1.length; _a++) {
+        var v = face_1[_a];
+        x += v.x;
+        y += v.y;
+        z += v.z;
+        i++;
+    }
+    return new Vertex(x / i, y / i, z / i);
+}
+function reorderFacesByPosition(faces) {
+    var faces = faces.slice(); // copy
+    var f = new Array();
+    var front;
+    var medians = new Array();
+    for (var _a = 0, faces_1 = faces; _a < faces_1.length; _a++) {
+        var face = faces_1[_a];
+        medians.push({ face: face, median: getFaceMedianPosition(face.face) });
+    }
+    while (medians.length != 0) {
+        front = medians[0];
+        for (var _b = 0, medians_1 = medians; _b < medians_1.length; _b++) {
+            var face_m = medians_1[_b];
+            if (front.median.y < face_m.median.y) {
+                front = face_m;
+            }
+        }
+        f.push(medians.splice(medians.indexOf(front), 1)[0].face);
+    }
+    return f.reverse();
+}
+var Color = /** @class */ (function () {
+    function Color(color) {
+        this.color = color;
+    }
+    Color.fromRGB = function (r, g, b) {
+        return new Color('rgb(' + r + ',' + g + ',' + b + ')');
+    };
+    return Color;
+}());
+function faceEquals(faceA, faceB) {
+    for (var i in faceA) {
+        if (faceA[i].x != faceB[i].x &&
+            faceA[i].y != faceB[i].y &&
+            faceA[i].z != faceB[i].z) {
+            return false;
+        }
+    }
+    return true;
+}
 var Obj3D = /** @class */ (function () {
     function Obj3D() {
+        this.vertices = new Array();
+        this.faces = new Array();
     }
-    Obj3D.translate = function (obj, to) {
-        for (var i = 0; i < obj.vertices.length; i++) {
-            obj.vertices[i].x += to.x;
-            obj.vertices[i].y += to.y;
-            obj.vertices[i].z += to.z;
+    Obj3D.asTranslate = function (obj, to) {
+        // Create new obj (COPY)
+        var _obj = new Obj3D();
+        _obj.faces = obj.faces.slice();
+        _obj.vertices = this._translate(obj, to);
+        return _obj;
+    };
+    Obj3D._translate = function (obj, to) {
+        var _vertices = obj.vertices.slice(); // copy
+        for (var i = 0; i < _vertices.length; i++) {
+            _vertices[i].x += to.x;
+            _vertices[i].y += to.y;
+            _vertices[i].z += to.z;
         }
+        return _vertices;
+    };
+    Obj3D.translate = function (obj, to) {
+        obj.vertices = this._translate(obj, to);
+    };
+    Obj3D.rotate = function (obj, center, theta, phi) {
+        var v;
+        for (var i = 0; i < obj.vertices.length; i++) {
+            v = obj.vertices[i];
+            // Rotation matrix coefficients
+            var ct = Math.cos(theta);
+            var st = Math.sin(theta);
+            var cp = Math.cos(phi);
+            var sp = Math.sin(phi);
+            // Rotation
+            var x = v.x - center.x;
+            var y = v.y - center.y;
+            var z = v.z - center.z;
+            v.x = ct * x - st * cp * y + st * sp * z + center.x;
+            v.y = st * x + ct * cp * y - ct * sp * z + center.y;
+            v.z = sp * y + cp * z + center.z;
+        }
+    };
+    Obj3D.getCenterByPosition = function (obj, position) {
+        var mX, mY, mZ;
+        var n = obj.vertices.length;
+        for (var _a = 0, _b = obj.vertices; _a < _b.length; _a++) {
+            var v = _b[_a];
+            mX += v.x + position.x;
+            mY += v.y + position.y;
+            mZ += v.z + position.z;
+        }
+        return new Vertex(mX / n, mY / n, mZ / n);
     };
     Obj3D.newCube = function (center, size) {
         // Generate the vertices
@@ -124,12 +221,18 @@ var Obj3D = /** @class */ (function () {
         ];
         // Generate the faces
         obj.faces = [
-            [obj.vertices[0], obj.vertices[1], obj.vertices[2], obj.vertices[3]],
-            [obj.vertices[3], obj.vertices[2], obj.vertices[5], obj.vertices[4]],
-            [obj.vertices[4], obj.vertices[5], obj.vertices[6], obj.vertices[7]],
-            [obj.vertices[7], obj.vertices[6], obj.vertices[1], obj.vertices[0]],
-            [obj.vertices[7], obj.vertices[0], obj.vertices[3], obj.vertices[4]],
-            [obj.vertices[1], obj.vertices[6], obj.vertices[5], obj.vertices[2]]
+            { face: [obj.vertices[0], obj.vertices[1], obj.vertices[2], obj.vertices[3]],
+                shader: { color: new Color('white') } },
+            { face: [obj.vertices[3], obj.vertices[2], obj.vertices[5], obj.vertices[4]],
+                shader: { color: new Color('white') } },
+            { face: [obj.vertices[4], obj.vertices[5], obj.vertices[6], obj.vertices[7]],
+                shader: { color: new Color('white') } },
+            { face: [obj.vertices[7], obj.vertices[6], obj.vertices[1], obj.vertices[0]],
+                shader: { color: new Color('white') } },
+            { face: [obj.vertices[7], obj.vertices[0], obj.vertices[3], obj.vertices[4]],
+                shader: { color: new Color('white') } },
+            { face: [obj.vertices[1], obj.vertices[6], obj.vertices[5], obj.vertices[2]],
+                shader: { color: new Color('white') } },
         ];
         return obj;
     };
@@ -139,11 +242,22 @@ var Vertex2D = /** @class */ (function () {
     function Vertex2D(x, y) {
         this.x = x;
         this.y = y;
+        this.x = x;
+        this.y = y;
     }
     return Vertex2D;
 }());
-function project(M) {
-    return new Vertex2D(M.x, M.z);
+var Camera = /** @class */ (function () {
+    function Camera(position) {
+        this.position = position;
+    }
+    return Camera;
+}());
+function project(M, camera, size) {
+    var x, y, r, d;
+    x = M.x - camera.position.x + size.w / 2;
+    y = M.z - camera.position.z + size.h / 2;
+    return new Vertex2D(x - camera.position.x, y - camera.position.y);
 }
 var Canvas = /** @class */ (function () {
     function Canvas(id, w, h) {
@@ -165,14 +279,8 @@ var Canvas = /** @class */ (function () {
     Canvas.prototype.self = function () {
         return document.querySelector("#" + this.id);
     };
-    Canvas.prototype._self = function () {
-        return document.getElementById(this.id);
-    };
-    Canvas.prototype.__self = function () {
-        return document.getElementById(this.id);
-    };
     Canvas.prototype.draw = function (frame) {
-        var canvasx = this._self().getContext('2d');
+        var canvasx = this.self().getContext('2d');
         for (var _a = 0, _b = frame.frame; _a < _b.length; _a++) {
             var pixel = _b[_a];
             canvasx.beginPath();
@@ -183,30 +291,92 @@ var Canvas = /** @class */ (function () {
         this.last = frame;
         return frame;
     };
-    Canvas.prototype.draw3D = function (objects, dx, dy) {
-        var ctx = this._self().getContext('2d');
-        var face, P;
+    Canvas.prototype.getMousePos = function () {
+        return this.mouse;
+    };
+    Canvas.prototype.draw3D = function (objects, pos, camera) {
+        var ctx = this.self().getContext('2d');
+        ctx.fillStyle = 'white';
+        var size = { w: this.w, h: this.h };
+        var face, P, hasShader;
         // For each object
         for (var i = 0, n_obj = objects.length; i < n_obj; ++i) {
+            // Don't render objects that aren't in front of the camera
+            // if(Obj3D.getCenterByPosition(objects[i], pos).y < camera.position.y){
+            //     continue;
+            // }
+            objects[i].faces = reorderFacesByPosition(objects[i].faces);
             // For each face
             for (var j = 0, n_faces = objects[i].faces.length; j < n_faces; ++j) {
                 // Current face
-                face = objects[i].faces[j];
+                face = objects[i].faces[j].face;
                 // Draw the first vertex
-                P = project(face[0]);
+                P = project(face[0], camera, size);
                 ctx.beginPath();
-                ctx.moveTo(P.x + dx, -P.y + dy);
+                ctx.moveTo(P.x + pos.x, P.y + pos.z);
                 // Draw the other vertices
                 for (var k = 1, n_vertices = face.length; k < n_vertices; ++k) {
-                    P = project(face[k]);
-                    ctx.lineTo(P.x + dx, -P.y + dy);
+                    P = project(face[k], camera, size);
+                    ctx.lineTo(P.x + pos.x, P.y + pos.z);
                 }
                 // Close the path and draw the face
                 ctx.closePath();
-                ctx.stroke();
+                ctx.fillStyle = objects[i].faces[j].shader.color.color;
+                // ctx.stroke();
                 ctx.fill();
             }
         }
+    };
+    Canvas.prototype.render = function (func, fps) {
+        var delta;
+        var must_pass = 1000 / fps;
+        var last_time = Date.now();
+        var _this = this;
+        // MOUSE OPERATIONS
+        this.mouse = { x: 0, y: 0 };
+        window.addEventListener('mousemove', function (evt) {
+            var rect = _this.self().getBoundingClientRect();
+            _this.mouse = {
+                x: evt.clientX - rect.left,
+                y: evt.clientY - rect.top
+            };
+            _this.mouse.x = _this.mouse.x > _this.w ? _this.w : _this.mouse.x;
+            _this.mouse.y = _this.mouse.y > _this.h ? _this.h : _this.mouse.y;
+            _this.mouse.x = _this.mouse.x < 0 ? 0 : _this.mouse.x;
+            _this.mouse.y = _this.mouse.y < 0 ? 0 : _this.mouse.y;
+        }, false);
+        function rerender() {
+            return __awaiter(this, void 0, void 0, function () {
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            _this.clearAll();
+                            delta = Date.now() - last_time;
+                            last_time = Date.now();
+                            func(delta / 1000); // delta is passed in 1.0 scale
+                            if (!(delta < must_pass)) return [3 /*break*/, 2];
+                            return [4 /*yield*/, sleep((must_pass - delta))];
+                        case 1:
+                            _a.sent();
+                            _a.label = 2;
+                        case 2:
+                            _this.raf = window.requestAnimationFrame(rerender);
+                            return [2 /*return*/];
+                    }
+                });
+            });
+        }
+        _this.raf = window.requestAnimationFrame(rerender);
+    };
+    Canvas.prototype.stopRender = function () {
+        try {
+            window.cancelAnimationFrame(this.raf);
+        }
+        catch (Exception) {
+            return false;
+        }
+        window.removeEventListener('mousemove');
+        return true;
     };
     Canvas.prototype.clear = function (frame) {
         var canvasx = this.self().getContext("2d");
@@ -228,18 +398,18 @@ var Canvas = /** @class */ (function () {
         return true;
     };
     Canvas.prototype.clearAll = function () {
-        this.__self().getContext("2d").clearRect(0, 0, this.__self().width, this.__self().height);
+        this.self().getContext("2d").clearRect(0, 0, this.self().width, this.self().height);
         return true;
     };
     return Canvas;
 }());
-var Animation = /** @class */ (function () {
-    function Animation(animationTime, _function) {
+var PFAnimation = /** @class */ (function () {
+    function PFAnimation(animationTime, _function) {
         this.animationTime = animationTime;
         this._function = _function;
     }
     ;
-    Animation.prototype.play = function () {
+    PFAnimation.prototype.play = function () {
         var self = this;
         self._animationTime = self.animationTime;
         self._inter = setInterval(loop_function, self._animationTime);
@@ -256,7 +426,7 @@ var Animation = /** @class */ (function () {
         ;
     };
     ;
-    return Animation;
+    return PFAnimation;
 }());
 ;
 var CanvasAnimation2D = /** @class */ (function () {
@@ -344,3 +514,6 @@ function rand(x) {
 }
 function rands(x) { return rand(x).toString(); }
 ;
+function toRad(g) {
+    return (g * Math.PI) / 180;
+}
